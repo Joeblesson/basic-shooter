e53994a849580b5b5d476148daa2e70fcabfb02e
Joeblesson <johanblesson@gmail.com>
2023-04-14 20:56:39 +0200

    sprites
    added some sprites

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML is cool!");
-    sf::CircleShape shape(250.f);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML is cool!", sf::Style::Fullscreen);
+    
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed) window.close();
+            if (event.key.code == sf::Keyboard::Escape) window.close(); 
+            
         }
+        //physics
 
+        //render
         window.clear();
-        window.draw(shape);
+       // window.draw();
         window.display();
     }
 
